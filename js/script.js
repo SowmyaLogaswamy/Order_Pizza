9d99fa1f09a5f7e7448546a651e9d0113b2a8b79
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-06-16 11:16:24 -0700

    Add functions in js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Business Logic
-function Pizza(name, toppings, pizzaSize,cost) {
+function Pizza(name, toppings,size,cost) {
   this.pizzaName = name;
   this.toppings = toppings;
   this.pizzaSize = size;
@@ -9,17 +9,35 @@ function Order(name, address) {
   this.customerName = name;
   this.address = address;
 }
-var pizzaToppingsArray=[];
+
+Pizza.prototype.pizzaRate = function (){
+  if(name === "1" && size==="small") {
+        cost = (name*size);
+      }
+      else if(name === "2" && size==="medium") {
+        chosenPizzaRate*mediumSizeCost;
+      }
+      else if(name === "3" && size==="large") {
+        chosenPizzaRate*largeSizeCost;
+      }
+
+}
+
 //User Interface Logic
 $(document).ready(function() {
   $("form#pizzaForm").submit(function(event) {
   event.preventDefault();
+  var pizzaToppingsArray=[];
   var pizzaName = $("input:radio[name=flavor]:checked").val();
-  var toppings = $("#toppings").val();
+  //var toppings = $("#toppings").val();
   $("input:checkbox[name=topping]:checked").each(function(){
       pizzaToppingsArray.push($(this).val());
   });
+  var pizzaSize = $("input:radio[name=size]:checked").val();
 console.log("test");
-
+var smallsizeCost;
+var newPizzaOrder = new Pizza(pizzaSize, pizzaName, pizzaToppingsArray);
+    var pizzaCost = newPizzaOrder.pizzaRate();
+    console.log("test1");
 });
 });
